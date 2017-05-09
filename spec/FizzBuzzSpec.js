ba40452704e4b8c58c3af550cc3a7b9f1776d366
Filipe Fasolin <ffasolin@gmail.com>
2017-05-10 00:28:36 +0100

    FizzBuzz functions prints array with 100 numbers

---
+++ spec/FizzBuzzSpec.js
@@ -0,0 +1,5 @@
+describe('FizBuzz', function() {
+  it('prints numbers 1 to 100', function() {
+    expect(FizzBuzz()).toContain(100);
+  });
+});
---
+++ src/FizzBuzz.js
@@ -0,0 +1,7 @@
+function FizzBuzz() {
+  for (i = 0; i <= 100; i++) {
+    arr = []
+    arr.push(i);
+  }
+  return arr
+}
