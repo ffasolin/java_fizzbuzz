ab17aa86f6b86fde93b21fe8dd1edfc55eff6dd5
Filipe Fasolin <ffasolin@gmail.com>
2017-05-10 12:48:28 +0100

    Solves fizzbuzz

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -1,5 +1,25 @@
 describe('FizBuzz', function() {
+ var fizzBuzz;
+
+  beforeEach(function() {
+    fizzBuzz = new FizzBuzz();
+  });
+
   it('prints numbers 1 to 100', function() {
-    expect(FizzBuzz()).toContain(100);
+    fizzBuzz.printNumbers()
+    expect(fizzBuzz.arr[3]).toContain('Fizz');
+    expect(fizzBuzz.arr[15]).toContain('FizzBuzz');
+    });
+
+  it('knows when number is divisible by three', function() {
+    expect(fizzBuzz.isDivisibleByThree(3)).toBe(true);
+  });
+
+  it('knows when number is divisible by five', function() {
+    expect(fizzBuzz.isDivisibleByFive(5)).toBe(true);
+  });
+
+  it('knows when number is divisible by three and five', function() {
+    expect(fizzBuzz.isDivisibleByThreeAndFive(15)).toBe(true);
   });
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,7 +1,39 @@
 function FizzBuzz() {
-  for (i = 0; i <= 100; i++) {
-    arr = []
-    arr.push(i);
+  this.printNumbers = function() {
+    this.arr = [];
+    for (i = 0; i <= 100; i++) {
+      if (this.isDivisibleByThreeAndFive(i)) {
+        this.arr.push('FizzBuzz');
+        console.log('FizzBuzz');
+      } else if (this.isDivisibleByFive(i)) {
+        this.arr.push('Buzz');
+        console.log('Buzz');
+      } else if (this.isDivisibleByThree(i)) {
+        this.arr.push('Fizz');
+        console.log('Fizz');
+      } else {
+        this.arr.push(i);
+        console.log(i);
+      }
+    }
+    return this.arr;
   }
-  return arr
-}
+
+  this.isDivisibleByThree = function(n) {
+    if (n % 3 === 0) {
+      return true;
+    }
+  }
+
+  this.isDivisibleByFive = function(n) {
+   if (n % 5 === 0) {
+     return true;
+   }
+  }
+
+  this.isDivisibleByThreeAndFive = function(n) {
+    if (n % 3 === 0 && n % 5 === 0) {
+      return true;
+    }
+  }
+};
